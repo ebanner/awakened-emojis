eb203fa57e4379cc96e1660b013b7918aaa803c0
Edward Banner <edward.banner@gmail.com>
2024-09-11 12:06:34 -0400

    Update lambda function

--- api/lambda_function.py
+++ api/lambda_function.py
@@ -20,6 +20,14 @@ users_and_channels = json.load(open('users_and_channels.json'))
 
 emoji_data = json.load(open('emojis.json'))
 
+EMOJIS = json.load(open('emojis.json'))
+
+related_emojis = json.load(open('related_emojis.json'))
+
+emoji_upload_data = json.load(open('emoji_upload_data.json'))
+
+emoji_popularity = json.load(open('emoji_popularity.json'))
+
 
 def get_path(event):
     path = event['requestContext']['http']['path']
@@ -50,29 +58,18 @@ def get_metadata(emoji):
 
 
 def get_usage(emoji):
-    print('IN GET_USAGE')
     df.ts = pd.to_datetime(df.ts, unit='s')
-    print('CONVERTED TS')
     emoji_df = df[df.emoji == emoji].reset_index(drop=True)
-    print('FILTERED DF')
-    count_df = emoji_df[['emoji', 'ts']].set_index('ts').resample('ME').count()
-    print('RESAMPLED')
+    count_df = emoji_df[['emoji', 'ts']].set_index('ts').resample('M').count()
     count_df = count_df.rename(columns={'emoji': 'count'})
-    print('RENAMED')
     count_df = count_df.reset_index()
-    print('RESET_INDEX')
     count_df.ts = count_df.ts.map(lambda ts: int(ts.timestamp()))
-    print('MAPPED TS')
     usage = count_df.to_dict(orient='records')
-    print('TO_DICT')
     return usage
 
 
 def get_usage_sqlite(emoji):
-    start = time.time()
     conn = sqlite3.connect('mydatabase.db')
-    end = time.time()
-    print('sqlite3.connect', end-start)
 
     query = f"""
 
@@ -86,31 +83,14 @@ ORDER BY month;
 
     """
 
-    start = time.time()
     df = pd.read_sql_query(query, conn, params=(emoji,))
-    end = time.time()
-    print('pd.read_sql_query', end-start)
 
-    start = time.time()
     df['ts'] = df['month'].apply(lambda ts: int(pd.to_datetime(ts).timestamp()))
-    end = time.time()
-    print('pd.to_datetime', end-start)
 
-    start = time.time()
     df.drop('month', axis=1, inplace=True)
-    end = time.time()
-    print('df.drop', end-start)
 
-    start = time.time()
     usage = df.to_dict(orient='records')
-    end = time.time()
-    print('df.to_dict', end-start)
-
-    start = time.time()
     conn.close()
-    end = time.time()
-    print('conn.close', end-start)
-
     return usage
 
 
@@ -147,9 +127,43 @@ def lambda_handler(event, context):
             'headers': CORS_HEADERS,
             'body': json.dumps(payload),
         }
-    elif action == 'data':
-        result = emoji_data[emoji]
+    elif action == 'related':
+        related = related_emojis[emoji]
+        related = [{**EMOJIS[emoji], 'name': emoji} for emoji in related]
+        payload = {
+            'name': emoji,
+            'related': related,
+        }
+        return {
+            'statusCode': 200,
+            'headers': CORS_HEADERS,
+            'body': json.dumps(payload),
+        }
+    elif action == 'upload_data':
+        result = emoji_upload_data[emoji]
         payload = result
+        return {
+            'statusCode': 200,
+            'headers': CORS_HEADERS,
+            'body': json.dumps(payload),
+        }
+    elif action == 'popularity':
+        result = emoji_popularity[emoji]
+        payload = {
+            'name': emoji,
+            'popularity': result
+        }
+        return {
+            'statusCode': 200,
+            'headers': CORS_HEADERS,
+            'body': json.dumps(payload),
+        }
+    elif action == 'basic_info':
+        emoji_info = EMOJIS[emoji]
+        payload = {
+            'name': emoji,
+            **emoji_info
+        }
         return {
             'statusCode': 200,
             'headers': CORS_HEADERS,
