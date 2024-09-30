6ad96ad6bc9ede839c9545d7eea7e58954087750
Edward Banner <edward.banner@gmail.com>
2024-09-30 12:15:59 -0400

    Get rid of old job to get emoji upload data

--- python/jobs.py
+++ python/jobs.py
@@ -7,42 +7,6 @@ from tqdm import tqdm
 import json
 
 
-def get_emoji_upload_data():
-    """
-
-    Highlight all the emojis on the customize slack page and copy and paste them
-    into a text file called emojis.txt.
-
-    """
-    
-    with open('emojis.txt') as f:
-        lines = [line.strip() for line in f.readlines()]
-
-    emojis = {}
-    for i in range(len(lines)):
-        if lines[i].startswith(':') and lines[i].endswith(':'):
-            emoji = lines[i]
-            i += 1
-            date = lines[i]
-            i += 1
-            while lines[i] == '':
-                i += 1
-            user = lines[i]
-            i += 1
-            while i < len(lines) and lines[i] == '':
-                i += 1
-            if i < len(lines):
-                assert not lines[i].startswith(':')
-            i += 1
-
-            emoji_name = emoji[1:-1] # strip off colons
-            emojis[emoji_name] = {
-                'date_added': date,
-                'added_by': user
-            }
-    return emojis
-
-
 def get_related_emojis():
     emojis = get_emojis()
 
@@ -112,9 +76,6 @@ def get_emojis():
 if __name__ == '__main__':
     # users_and_channels = get_users_and_channels()
     # json.dump(users_and_channels, open('users_and_channels.json', 'w'))
-
-    # emoji_upload_data = get_emoji_upload_data()
-    # json.dump(emoji_upload_data, open('emoji_upload_data.json', 'w'))
 
     related_emojis = get_related_emojis()
     json.dump(related_emojis, open('related_emojis.json', 'w'))
