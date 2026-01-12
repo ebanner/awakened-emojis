bea8cdda6ef353d519b1c9765ddfb5fdd11eff7d
Edward Banner <edward@Mac.lan>
2026-01-12 11:13:31 -0500

    Add user column

--- python/get_youtube_messages.py
+++ python/get_youtube_messages.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 def get_youtube_messages():
     def _get_youtube_messages(path):
         messages = json.load(open(path))
-        _youtube_messages = [message for message in messages if 'youtube' in message.get('text', '')]
+        _youtube_messages = [message for message in messages if 'attachments' in message and any(attachment.get('service_name') == 'YouTube' for attachment in message['attachments'])]
         return _youtube_messages
 
     paths = get_ipython().getoutput('ls data/awakened_zip/music/*')
@@ -24,7 +24,18 @@ def get_youtube_messages():
 if __name__ == '__main__':
     youtube_messages = get_youtube_messages()
 
-    df = pd.DataFrame((youtube_message['user'], youtube_message['text']) for youtube_message in youtube_messages)
+    songs = []
+    for youtube_message in youtube_messages:
+        song = [
+            (
+                youtube_message['user'],
+                attachment['title'],
+                attachment['title_link']
+            ) for attachment in youtube_message['attachments']
+        ]
+        songs.extend(song)
+
+    df = pd.DataFrame(songs, columns=['user', 'title', 'link'])
 
     print(df)
 
